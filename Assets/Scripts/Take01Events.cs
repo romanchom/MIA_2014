1dc7c2391a1badd35ec68134d500eb1ca2a25132
Luke Nizik <elejdor@gmail.com>
2015-01-13 19:11:04 +0100

    [+] a script and event to put down a cup

---
+++ Assets/Scripts/Take01Events.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Take01Events : MonoBehaviour {
+
+    [SerializeField]
+    GameObject cupOnBoard;
+
+    [SerializeField]
+    GameObject cupInHand;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    void PutDownCup()
+    {
+        Destroy(cupInHand);
+        cupOnBoard.SetActive(true);
+    }
+}
