e0e485f1d4f77d204f2e3b1dcabe8dfd1392c650
romanchom <romanchom@gmail.com>
2015-01-20 21:40:58 +0100

    Zmarnowałem godzinę mojego życia na otwieranie i zamykanie portalu.
    Ja pierdole

---
+++ Assets/Scripts/DelayTrigger.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DelayTrigger : MonoBehaviour {
+	public Animator anim;
+	public float delay;
+	public string name;
+	void Start() {
+		StartCoroutine(Fire());
+	}
+
+	IEnumerator Fire() {
+		yield return new WaitForSeconds(delay - Time.time);
+		anim.SetTrigger(name);
+	}
+}
