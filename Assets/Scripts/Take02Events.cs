b087b7b9197d35956073b3e7fefc754feb75f000
Luke Nizik <elejdor@gmail.com>
2015-01-21 08:08:14 +0100

    [+] animation of falling down after getting hit by a BM guy

---
+++ Assets/Scripts/Take02Events.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Take02Events : MonoBehaviour {
+
+    [SerializeField]
+    GameObject firstChar;
+
+    [SerializeField]
+    GameObject secondChar;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void StandUpZabijMnieSmiechem()
+    {
+        firstChar.SetActive(false);
+        secondChar.SetActive(true);
+    }
+}
