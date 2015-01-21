fd25d7a61b020963fbc59d024232bfb942dcc2ac
Luke Nizik <elejdor@gmail.com>
2015-01-21 11:03:13 +0100

    [+] bubbles

---
+++ Assets/Scripts/BubbleScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BubbleScript : MonoBehaviour {
+
+    Animator bubbleAnimator;
+
+	// Use this for initialization
+	void Start () {
+        bubbleAnimator = GetComponentInChildren<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetKeyUp("q"))
+        {
+            ShowBubble();
+        }
+        else if (Input.GetKeyUp("w"))
+        {
+            HideBubble();
+        }
+	}
+
+    public void ShowBubble()
+    {
+        bubbleAnimator.SetTrigger("Show");
+    }
+
+    public void HideBubble()
+    {
+        bubbleAnimator.SetTrigger("Hide");
+    }
+
+}
